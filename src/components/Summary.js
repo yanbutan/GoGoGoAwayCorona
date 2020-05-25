a45badaba21cd7e800686b6b67a354920a21b74d
yanbutan <ynaung6995@gmail.com>
2020-05-25 23:42:51 +0800

    incorporated useReducer, kinda messy but lazy to clean up

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,102 +1,60 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet, ActivityIndicator} from 'react-native';
 import HeaderStyle from 'src/assets/jss/HeaderStyle';
-import MaskWomanSVG from 'src/assets/svg/mask-woman.svg';
-import DoctorWomanSVG from 'src/assets/svg/doctor-woman.svg';
-import RemoteWorkSVG from 'src/assets/svg/remote-work-woman.svg';
-import {useFetch} from 'src/hooks/useFetch';
 const useStyles = StyleSheet.create(HeaderStyle);
 const styles = useStyles();
 
-export default function Summary() {
-  const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(true);
-  // const fetchResponse = useFetch(
-  //   'https://corona.lmao.ninja/v2/countries/Singapore?yesterday&strict&query',
-  //   {isLoading: true, data: null},
-  // );
-  // setData(fetchResponse.data);
-  useEffect(() => {
-    fetch(
-      'https://corona.lmao.ninja/v2/countries/Singapore?yesterday&strict&query',
-    )
-      .then(res => res.json())
-      .then(json => setData(json))
-      .catch(error => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
+export default function Summary(props) {
+  const {data} = props;
 
   return (
     <>
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
+      {data && (
         <>
-        <View style={[styles.horizontalContain,{marginRight: 6, marginLeft: 6}]}>
           <View
-            style={[
-              styles.card,
-              styles.shadow,
-              styles.death
-            ]}>
-            <View>
-              <Text style={[styles.cardPrimary, {color: "#F98C62"}]}>{data.cases}</Text>
-              <Text style={[styles.cardSecondary, {color: "#FBB192"}]}>Confirmed Cases</Text>
+            style={[styles.horizontalContain, {marginRight: 6, marginLeft: 6}]}>
+            <View style={[styles.card, styles.shadow, styles.death]}>
+              <View>
+                <Text style={[styles.cardPrimary, {color: '#F98C62'}]}>
+                  {data.cases}
+                </Text>
+                <Text style={[styles.cardSecondary, {color: '#FBB192'}]}>
+                  Confirmed Cases
+                </Text>
+              </View>
             </View>
-            {/* <View>
-              <MaskWomanSVG width={100} height={90} />
-            </View> */}
-          </View>
-          <View
-            style={[
-              styles.card,
-              styles.shadow,
-              styles.death,
-            ]}>
-            {/* <View>
-              <DoctorWomanSVG width={100} height={90} />
-            </View> */}
-            <View>
-              <Text style={[styles.cardPrimary, {color: '#A3003F'}]}>
-                {data.deaths}
-              </Text>
-              <Text style={[styles.cardSecondary, {color: '#FF5959'}]}>
-                Deaths
-              </Text>
+            <View style={[styles.card, styles.shadow, styles.death]}>
+              <View>
+                <Text style={[styles.cardPrimary, {color: '#A3003F'}]}>
+                  {data.deaths}
+                </Text>
+                <Text style={[styles.cardSecondary, {color: '#FF5959'}]}>
+                  Deaths
+                </Text>
+              </View>
             </View>
-          </View>
-        </View>
-        <View style={[styles.horizontalContain,{marginRight: 6, marginLeft: 6}]}>
-           <View
-            style={[
-              styles.card,
-              styles.shadow,
-              styles.recovered,
-            ]}>
-            <View>
-              <Text style={styles.cardPrimary}>{data.recovered}</Text>
-              <Text style={styles.cardSecondary}>Recovered</Text>
-            </View>
-            {/* <View>
-              <RemoteWorkSVG width={100} height={90} />
-            </View> */}
-            <View />
           </View>
           <View
-            style={[
-              styles.card,
-              styles.shadow,
-            ]}>
-            <View>
-              <Text style={[styles.cardPrimary,{color: "#2D31AC"}]}>{data.recovered}</Text>
-              <Text style={[styles.cardSecondary, {color: "#6587DE"}]}>Recovered</Text>
+            style={[styles.horizontalContain, {marginRight: 6, marginLeft: 6}]}>
+            <View style={[styles.card, styles.shadow, styles.recovered]}>
+              <View>
+                <Text style={styles.cardPrimary}>{data.recovered}</Text>
+                <Text style={styles.cardSecondary}>Recovered</Text>
+              </View>
+              <View />
             </View>
-            {/* <View>
-              <RemoteWorkSVG width={100} height={90} />
-            </View> */}
-            <View />
+            <View style={[styles.card, styles.shadow]}>
+              <View>
+                <Text style={[styles.cardPrimary, {color: '#2D31AC'}]}>
+                  {data.active}
+                </Text>
+                <Text style={[styles.cardSecondary, {color: '#6587DE'}]}>
+                  Active
+                </Text>
+              </View>
+              <View />
+            </View>
           </View>
-        </View>
         </>
       )}
     </>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer, useEffect} from 'react';
 import {
   ScrollView,
   StyleSheet,
   View,
   Text,
   TouchableOpacity,
+  ActivityIndicator,
 } from 'react-native';
 import Header from 'src/components/Header';
 import Summary from 'src/components/Summary';
@@ -13,46 +14,102 @@ import LatestNews from 'src/components/LatestNews';
 import HeaderStyle from 'src/assets/jss/HeaderStyle';
 import {Picker} from '@react-native-community/picker';
 import {NEWS_API_KEY} from 'react-native-dotenv';
+import axios from 'axios';
 
 const useStyles = StyleSheet.create(HeaderStyle);
 const styles = useStyles();
 
 let pickerData = ['Singapore', 'Worldwide'];
+const initialState = {
+  loading: true,
+  error: null,
+  summaryData: {},
+  trendData: {},
+  newsData: {},
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SUCCESS':
+      return {
+        loading: false,
+        summaryData: action.fetchedSummary,
+        // trendData: action.fetchedTrend,
+        // newsData: action.fetchedNews
+      };
+
+    case 'ERROR':
+      return {
+        loading: false,
+        error: action.errorMsg,
+        summaryData: action.fetchedSummary,
+        // trendData: action.fetchedTrend,
+        // newsData: action.fetchedNews
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default function Home() {
   const [selectedRegion, setSelectedRegion] = useState('singapore');
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    axios
+      .all([
+        axios.get(
+          'https://corona.lmao.ninja/v2/countries/Singapore?yesterday&strict&query',
+        ),
+      ])
+      .then(fetchedSummary => {
+        dispatch({type: 'SUCCESS', fetchedSummary: fetchedSummary[0].data});
+      })
+      .catch(error => {
+        dispatch({type: 'ERROR', errorMsg: error.message});
+      });
+  }, [selectedRegion]);
+
   return (
-    <ScrollView style={styles.container}>
-      <Header title="Covid Statistics" />
-      <View style={styles.section}>
-        <View style={styles.horizontalContain}>
-          <View style={styles.viewLeft}>
-            <Text style={styles.titleText}>Summary</Text>
+    <>
+      {state.loading ? (
+        <ActivityIndicator />
+      ) : (
+        <ScrollView style={styles.container}>
+          <Header title="Covid Statistics" />
+          <View style={styles.section}>
+            <View style={styles.horizontalContain}>
+              <View style={styles.viewLeft}>
+                <Text style={styles.titleText}>Summary</Text>
+              </View>
+              <View style={styles.viewRight}>
+                <TouchableOpacity style={[styles.picker, styles.shadow]}>
+                  <Picker
+                    style={styles.pickerLabel}
+                    selectedValue={selectedRegion}
+                    onValueChange={(itemValue, itemIndex) =>
+                      setSelectedRegion(itemValue)
+                    }>
+                    {pickerData.map(region => (
+                      <Picker.Item
+                        key={region}
+                        label={region}
+                        value={region}
+                        color="#1E6262"
+                      />
+                    ))}
+                  </Picker>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <Summary data={state.summaryData} />
           </View>
-          <View style={styles.viewRight}>
-            <TouchableOpacity style={[styles.picker, styles.shadow]}>
-              <Picker
-                style={styles.pickerLabel}
-                selectedValue={selectedRegion}
-                onValueChange={(itemValue, itemIndex) =>
-                  setSelectedRegion(itemValue)
-                }>
-                {pickerData.map(region => (
-                  <Picker.Item
-                    key={region}
-                    label={region}
-                    value={region}
-                    color="#1E6262"
-                  />
-                ))}
-              </Picker>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <Summary />
-      </View>
-      <CaseTrend />
-      <LatestNews />
-    </ScrollView>
+          <CaseTrend />
+          <LatestNews />
+        </ScrollView>
+      )}
+      {state.error ? state.error : null}
+    </>
   );
 }
