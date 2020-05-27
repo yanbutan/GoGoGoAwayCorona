722e393947fd9ab610d7c1547ad630e3f87bd069
yanbutan <ynaung6995@gmail.com>
2020-05-27 23:48:42 +0800

    fuck this shit

--- src/components/LatestNews.js
+++ src/components/LatestNews.js
@@ -1,34 +1,38 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import {View, Text, StyleSheet, ScrollView, ActivityIndicator} from 'react-native';
 import NewsStyle from 'src/assets/jss/NewsStyle';
 // import MaskWomanSVG from 'src/assets/svg/mask-woman.svg';
 const useStyles = StyleSheet.create(NewsStyle);
 const styles = useStyles();
 
-export default function LatestNews() {
-  return (
-    <View style={styles.section}>
-      <View style={styles.horizontalContain}>
-        <View style={styles.viewLeft}>
-          <Text style={styles.titleText}>Latest News</Text>
+export default function LatestNews(props) {
+  const {data, loading} = props;
+
+  if (data) {
+    return (
+      <View style={styles.section}>
+        <View style={styles.horizontalContain}>
+          <View style={styles.viewLeft}>
+            <Text style={styles.titleText}>Latest News</Text>
+          </View>
         </View>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          {data.map((article, index) => (
+            <View key={index} style={[styles.newsCard, styles.shadow]}>
+              <View style={styles.image} />
+              <View style={styles.newsSummary}>
+                <Text style={styles.newsPrimary}>{`${article.title.slice(
+                  0,
+                  20,
+                )}...`}</Text>
+                <Text style={styles.newsSecondary}>
+                  {article.publishedAt.slice(0, 9)}
+                </Text>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
       </View>
-      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-        <View style={[styles.newsCard, styles.shadow]}>
-          <View style={styles.image} />
-          <View style={styles.newsSummary}>
-            <Text style={styles.newsPrimary}>Some News Title</Text>
-            <Text style={styles.newsSecondary}>13 May 2019</Text>
-          </View>
-        </View>
-        <View style={[styles.newsCard, styles.shadow]}>
-          <View style={styles.image} />
-          <View style={styles.newsSummary}>
-            <Text style={styles.newsPrimary}>Some News Title</Text>
-            <Text style={styles.newsSecondary}>13 May 2019</Text>
-          </View>
-        </View>
-      </ScrollView>
-    </View>
-  );
+    );
+  } else return <ActivityIndicator />;
 }
